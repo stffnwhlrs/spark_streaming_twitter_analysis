fd17f352dd4390c42b1f60f92e1d43f69e4f9cc4
stffnwhlrs <steffenwohlers@gmx.de>
2021-04-14 13:01:21 +0200

    created shell script for kafka setup, edited readme

---
+++ setup_kafka_env.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Start Kafka
+sudo service kafka start
+
+# Kafka status
+systemctl is-active --quiet kafka && echo "Kafka is running."
+
+# Create topic twitterPublic
+#/opt/kafka/bin/kafka-topics.sh --create --topic twitterPublic --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --if-not-exists &&
+#echo "Kafka topic twitterPublic exists"
+
+
+# Create topics if not existing
+for topic in twitterPublic
+do
+  /opt/kafka/bin/kafka-topics.sh --create --topic $topic --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --if-not-exists &&
+  echo "Kafka topic ${topic} exists."
+done
+
+# List all existing topics
+# /opt/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181

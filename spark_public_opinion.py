574b59cabdce5f6abb88ded889eddc1f4e0fff87
stffnwhlrs <steffenwohlers@gmx.de>
2021-05-01 22:24:44 +0200

    first file

--- spark_public_opinion.py
+++ spark_public_opinion.py
@@ -1,25 +1,15 @@
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-
+from pyspark.streaming.kafka import KafkaUtils
+import json
 
 # create spark configuration
 sc = SparkContext("local[2]", "TwitterWordCount")
 sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 5)
 
-print("succes!!!!!!!!!!!")
-
-df = ssc \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", "twitterPublic") \
-  .load()
-
-print(df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)"))
-
-
+kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'publicOpinion':1})
 
 
 

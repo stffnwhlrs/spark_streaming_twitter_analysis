9d221e5528ac52cd0c4859e3ef36ac74975d0701
stffnwhlrs <steffenwohlers@gmx.de>
2021-05-01 22:43:37 +0200

    first file

--- spark_public_opinion.py
+++ spark_public_opinion.py
@@ -3,20 +3,24 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
-spark = SparkSession \
-    .builder \
-    .appName("StructuredNetworkWordCount") \
-    .getOrCreate()
+spark = SparkSession.builder\
+                    .appName('Tweet Sentiment Analysis')\
+                    .getOrCreate()
 
 
-# Subscribe to 1 topic
-df = spark \
+location = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "localhost:9092") \
   .option("subscribe", "twitterPublic") \
+  .option("startingOffsets", "latest") \
+  .option("failOnDataLoss", "false") \
   .load()
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+location = location.selectExpr("CAST(value AS STRING)")
+
+print("Are we streaming? " + str(location.isStreaming))
+
 
 if __name__ == "__main__":
     print("lol")

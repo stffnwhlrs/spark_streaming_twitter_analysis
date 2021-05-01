3bfd61521f7dffdb1d84cc88408c1307ada909c4
stffnwhlrs <steffenwohlers@gmx.de>
2021-05-01 22:34:13 +0200

    first file

--- spark_public_opinion.py
+++ spark_public_opinion.py
@@ -8,5 +8,15 @@ spark = SparkSession \
     .appName("StructuredNetworkWordCount") \
     .getOrCreate()
 
+
+# Subscribe to 1 topic
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "localhost:9092") \
+  .option("subscribe", "publicOpinion") \
+  .load()
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
 if __name__ == "__main__":
     print("lol")

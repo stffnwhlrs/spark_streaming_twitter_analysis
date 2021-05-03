1e046783784b141fe9251e1315c618fc889e56a4
stffnwhlrs <steffenwohlers@gmx.de>
2021-05-03 18:38:51 +0200

    first try create df

--- spark_public_opinion.py
+++ spark_public_opinion.py
@@ -1,13 +1,10 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split
-from pyspark.sql.functions import udf
-from time import sleep
-import re
-#from pyspark.streaming.kafka import KafkaUtils
 import pyspark.sql.functions as f
 from pyspark.sql.functions import lit
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.from_json
 
 
 spark = SparkSession.builder\
@@ -15,6 +12,11 @@ spark = SparkSession.builder\
                     .getOrCreate()
 
 spark.sparkContext.setLogLevel("ERROR")
+
+
+schema = StructType([ \
+  StructField("text", StringType(), True) \
+    ])
 
 
 raw_input = spark \
@@ -26,12 +28,15 @@ raw_input = spark \
   .option("failOnDataLoss", "false") \
   .load()
 
-#raw_input = raw_input.selectExpr("CAST(value AS STRING)")
+raw_input = raw_input.selectExpr("CAST(value AS STRING)"
 
 print("Are we streaming? " + str(raw_input.isStreaming))
 
+tweets = raw_input.select(from_json(raw_input.text, schema))
+
+
 print("Data Schema:")
-raw_input.printSchema()
+tweets.printSchema()
 
 
 # Start running the query that prints the running counts to the console
